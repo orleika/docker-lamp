de499cacd25e2f9fef8a800f11b8bb0a6b14f013
orleika <orleika.net@gmail.com>
2016-09-24 04:16:19 +0000

    Fix initdb

--- mysql/initdb.d/create_app_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-
-mysql=( mysql --protocol=socket -uroot -p"${MYSQL_ROOT_PASSWORD}" )
-database="app"
-
-"${mysql[@]}" <<-EOSQL
-  CREATE DATABASE IF NOT EXISTS ${database};
-  GRANT ALL ON ${database}.* TO '${MYSQL_USER}'@'%' ;
-EOSQL
